eda6c951eabe1f5c924687df6369e3db00e96d36
Daniel Amarante <daniel.amarante2@gmail.com>
2020-07-16 17:27:14 -0300

    Create empty example project

---
+++ Examples/JanusStreaming/JanusStreaming/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JanusStreaming
+//
+//  Created by Daniel Amarante on 7/16/20.
+//  Copyright © 2020 Alancorp. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
